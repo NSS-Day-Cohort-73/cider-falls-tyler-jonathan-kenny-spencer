aadceb6c92a3550221f341e1183619d7937e9280
Jonathan Castro <jonathancastro55@gmail.com>
2024-10-01 15:52:42 -0500

    Fixed event listener and finished geeGuestCount

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,17 +1,32 @@
-import { getAreas } from "./database.js";
+import { getAreas, getGuests } from "./database.js";
 
 export const generateAreas = () => {
     const areas = getAreas();
+    
     let areasHTML = "";
 
     for (const area of areas) {
         areasHTML += `
             <div class="area-box" data-id="${area.id}">
-                <h2>${area.name}</h2>
-                <p>Spot check!</p>
+                <h2 class="area-title">${area.name}</h2>
             </div>
         `;
     }
 
     return areasHTML;
 };
+
+const getGuestCount = (areaId) => {
+    const guests = getGuests(); 
+    return guests.filter(guest => guest.areaId === parseInt(areaId)).length; 
+};
+
+document.addEventListener("click", (event) => {
+
+    const clickedTitle = event.target.closest(".area-title");
+    if (clickedTitle) {
+        const areaId = clickedTitle.parentElement.dataset.id; 
+        const guestCount = getGuestCount(areaId); 
+        alert(`There are ${guestCount} guests in this area`);
+    }
+});
