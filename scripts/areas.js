7a8ea0f4bc50c2db9b073daa2530fad8c71ed100
Kenny Panyavong <elp.kenny@gmail.com>
2024-10-01 14:27:42 -0500

    Added areas click event to display number of guests

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,17 +1,31 @@
-import { getAreas } from "./database.js";
+import { getAreas } from "./database.js"
 
 export const generateAreas = () => {
-    const areas = getAreas();
-    let areasHTML = "";
+    const areas = getAreas()
+    
+    let areasHTML = ""
 
     for (const area of areas) {
         areasHTML += `
             <div class="area-box" data-id="${area.id}">
-                <h2>${area.name}</h2>
-                <p>Services: </p>
+                <h2 class="area-title">${area.name}</h2>
             </div>
-        `;
+        `
     }
 
-    return areasHTML;
-};
+    return areasHTML
+}
+
+
+const getGuestCount = () => {
+    return 10
+} //replace with function from guests.js
+
+document.addEventListener("click", (event) => {
+    const clickedTitle = event.target.closest(".area-title")
+    if (clickedTitle) {
+        const areaId = clickedTitle.parentElement.dataset.id
+        const guestCount = getGuestCount()
+        alert(`There are ${guestCount} guests in this area`)
+    }
+})
